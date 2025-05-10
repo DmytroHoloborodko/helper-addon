768784b00accc7df9cadb9c1d8d9f1c38e75267a
Dmytro Holoborodko <dmytro.holoborodko@gmail.com>
2025-05-10 21:31:32 +0200

    Hide micro menu and bags bar

---
+++ Hide_UI.lua
@@ -0,0 +1,35 @@
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:SetScript("OnEvent", function()
+    -- Hide Micro Menu (Character, Spellbook, etc.)
+    if MicroMenuContainer then
+        MicroMenuContainer:Hide()
+        MicroMenuContainer.Show = function()
+        end -- Prevent re-showing
+    end
+
+    -- Hide Backpack and Bag Slots
+    if MainMenuBarBackpackButton then
+        BagBarExpandToggle:Hide()
+        CharacterReagentBag0Slot:Hide()
+        MainMenuBarBackpackButton:Hide()
+        MainMenuBarBackpackButton.Show = function()
+        end
+
+        for i = 0, 3 do
+            local bag = _G["CharacterBag" .. i .. "Slot"]
+            if bag then
+                bag:Hide()
+                bag.Show = function()
+                end
+            end
+        end
+    end
+
+    -- Move the Queue Status Button
+    if QueueStatusButton then
+        QueueStatusButton:SetParent(UIParent)
+        QueueStatusButton:ClearAllPoints()
+        QueueStatusButton:SetPoint("BOTTOMLEFT", Minimap, "BOTTOMLEFT", 40, -15)
+    end
+end)
